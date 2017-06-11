fbc25836fbbe737aab3704d1d4939eb1fe96d95e
ITdian <chenjinbode@yeah.net>
2017-06-12 00:23:52 +0800

    Add Ajax

---
+++ Ajax/code/01-test.php
@@ -0,0 +1,30 @@
+<?php
+/*$number = 10;
+echo $number;*/
+
+
+//php数组,字典
+//$arr = array(1,2,3,4);
+//$dict = array('name'=>'ni','age'=>18);
+//print_r($arr);
+//print_r($dict);
+
+
+//$number = 19;
+//if($number>=18){
+//    echo '18';
+//}else{
+//    echo '2';
+//}
+
+//$age = 15;
+//$res = $age >= 18 ?18:2;
+//echo $res;
+$arr = array(1,2,3,4);
+for ($index = 0;$index<count($arr);$index++){
+    echo $arr[$index];
+    echo $index;
+    echo  '<br>';
+ }
+
+
---
+++ Ajax/code/02-get.php
@@ -0,0 +1,9 @@
+<?php
+
+//print_r($_GET);
+//注意点
+/*$_GET中的kry值必须和前端提交的key一模一样,否则会报错*/
+
+echo $_GET["userName"];
+echo "<br>";
+echo $_GET["password"];
